7c8c6182a6dc56cc284b4769428d670a19f215b5
sam1382 <samiiiiiiiiiiiiiiiiiiiiiiii24@gmail.com>
2016-06-17 00:58:39 -0700

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,20 +1,16 @@
-local function add_user(chat, user)
-   local status = chat_add_user (chat, user, ok_cb, false)
-   if not status then
-   end
-end
-    local function run(msg, matches)
-           if matches[1] == 'chat_del_user' then
-                local user = 'chat#id'..msg.to.id 
-                local chat = 'chat#id'..msg.from.id
-                add_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id)
-                --return "" -- You can put everything here for returing when someone is added !
+do
+local function run(msg, matches)
+local bot_id = 199063048
+local fbot1 = 187419654
+    if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
     end
 end
 
 return {
-    patterns = {
-        "^!!tgservice (chat_del_user)$",
-        },
-    run = run
+  patterns = {
+    "^!!tgservice (.+)$",
+  },
+  run = run
 }
+end
